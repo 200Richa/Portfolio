d12eb1e560317b4ac246bb3419fafcd9b0a679c6
Richa <shahiricha2412@gmail.com>
2022-01-18 11:43:28 +0530

    complete resume page with all styling

---
+++ app.js
@@ -0,0 +1,9 @@
+var firstScrollSpyEl = document.querySelector('[data-bs-spy="scroll"]')
+firstScrollSpyEl.addEventListener('activate.bs.scrollspy', function () {
+  // do something...
+  console.log("Scrolled")
+})
+
+
+// let fb = document.querySelectorAll(".socials")[0]
+// fb.addEventListener('click', )
